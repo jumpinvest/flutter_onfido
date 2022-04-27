dbc21453f8902b37e27d5024eb738f81911e8eda
Helio Padrao <hpadrao@gmail.com>
2022-04-27 21:37:36 +0100

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'dart:async';
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
@@ -21,7 +22,8 @@ class _MyAppState extends State<MyApp> {
       //     "sdk_sandbox_us.KnnlEbnTf1o.LzRSAHlbKV0hUNc7a4Owtd_JsPnqX6wj";
 
       const exampleSdkToken =
-          'eyJhbGciOiJFUzUxMiJ9.eyJleHAiOjE2NDQ0MjAwNTIsInBheWxvYWQiOnsiYXBwIjoiYzE1ZDI0MzUtZmJlMy00MDQzLWE4NWMtOGMzMmQ4OTA0MmZjIiwiY2xpZW50X3V1aWQiOiI2NWQ3NTQ1YS0zYzc5LTQxYjItOTE0YS0xMmUzMWQ4ZThlMGIiLCJhcHBsaWNhdGlvbl9pZCI6ImNvbS5mbHVlbmN5YmFuay5mbHV0dGVyX29uZmlkb19leGFtcGxlIiwiaXNfc2FuZGJveCI6dHJ1ZSwic2FyZGluZV9zZXNzaW9uIjoiNjI0ZmVlMTctYjgzNC00MjIxLWE5NDUtNmEwNzU5MWFmNTgxIn0sInV1aWQiOiJ1cmFwSU5JNWpOOCIsInVybHMiOnsidGVsZXBob255X3VybCI6Imh0dHBzOi8vdGVsZXBob255LnVzLm9uZmlkby5jb20iLCJkZXRlY3RfZG9jdW1lbnRfdXJsIjoiaHR0cHM6Ly9zZGsudXMub25maWRvLmNvbSIsInN5bmNfdXJsIjoiaHR0cHM6Ly9zeW5jLm9uZmlkby5jb20iLCJob3N0ZWRfc2RrX3VybCI6Imh0dHBzOi8vaWQub25maWRvLmNvbSIsImF1dGhfdXJsIjoiaHR0cHM6Ly9hcGkudXMub25maWRvLmNvbSIsIm9uZmlkb19hcGlfdXJsIjoiaHR0cHM6Ly9hcGkudXMub25maWRvLmNvbSJ9fQ.MIGHAkFH7Tnx0gZUe2sUL0QzJbXRANwN3ced9f3LrFFNXpLatkXNux5CVaqI-cQ3OWbL6ttLmHrRQetlRM3nHwi6eyJ0IAJCAKm2dBCXaPl7SHSP9ArzvYQExH0K6F_zsZytozTT44RnndGXmaAOhtU3e8MeJ3JRP-JHa9nnhRj7JBC3C1SSQE1C';
+          // 'eyJhbGciOiJFUzUxMiJ9.eyJleHAiOjE2NDQ0MjAwNTIsInBheWxvYWQiOnsiYXBwIjoiYzE1ZDI0MzUtZmJlMy00MDQzLWE4NWMtOGMzMmQ4OTA0MmZjIiwiY2xpZW50X3V1aWQiOiI2NWQ3NTQ1YS0zYzc5LTQxYjItOTE0YS0xMmUzMWQ4ZThlMGIiLCJhcHBsaWNhdGlvbl9pZCI6ImNvbS5mbHVlbmN5YmFuay5mbHV0dGVyX29uZmlkb19leGFtcGxlIiwiaXNfc2FuZGJveCI6dHJ1ZSwic2FyZGluZV9zZXNzaW9uIjoiNjI0ZmVlMTctYjgzNC00MjIxLWE5NDUtNmEwNzU5MWFmNTgxIn0sInV1aWQiOiJ1cmFwSU5JNWpOOCIsInVybHMiOnsidGVsZXBob255X3VybCI6Imh0dHBzOi8vdGVsZXBob255LnVzLm9uZmlkby5jb20iLCJkZXRlY3RfZG9jdW1lbnRfdXJsIjoiaHR0cHM6Ly9zZGsudXMub25maWRvLmNvbSIsInN5bmNfdXJsIjoiaHR0cHM6Ly9zeW5jLm9uZmlkby5jb20iLCJob3N0ZWRfc2RrX3VybCI6Imh0dHBzOi8vaWQub25maWRvLmNvbSIsImF1dGhfdXJsIjoiaHR0cHM6Ly9hcGkudXMub25maWRvLmNvbSIsIm9uZmlkb19hcGlfdXJsIjoiaHR0cHM6Ly9hcGkudXMub25maWRvLmNvbSJ9fQ.MIGHAkFH7Tnx0gZUe2sUL0QzJbXRANwN3ced9f3LrFFNXpLatkXNux5CVaqI-cQ3OWbL6ttLmHrRQetlRM3nHwi6eyJ0IAJCAKm2dBCXaPl7SHSP9ArzvYQExH0K6F_zsZytozTT44RnndGXmaAOhtU3e8MeJ3JRP-JHa9nnhRj7JBC3C1SSQE1C';
+          'eyJhbGciOiJFUzUxMiJ9.eyJleHAiOjE2NTEwOTI3NDEsInBheWxvYWQiOnsiYXBwIjoiYzE1ZDI0MzUtZmJlMy00MDQzLWE4NWMtOGMzMmQ4OTA0MmZjIiwiY2xpZW50X3V1aWQiOiI2NWQ3NTQ1YS0zYzc5LTQxYjItOTE0YS0xMmUzMWQ4ZThlMGIiLCJhcHBsaWNhdGlvbl9pZCI6ImNvbS5mbHVlbmN5YmFuay5mbHV0dGVyX29uZmlkb19leGFtcGxlIiwiaXNfc2FuZGJveCI6dHJ1ZSwic2FyZGluZV9zZXNzaW9uIjoiYjY4YTU1NzgtMjA3Zi00ZWI2LWE5NWMtODA0MzAzOGQ4OGZmIn0sInV1aWQiOiJQRmwyR0o2LWRlNCIsInVybHMiOnsidGVsZXBob255X3VybCI6Imh0dHBzOi8vdGVsZXBob255LnVzLm9uZmlkby5jb20iLCJkZXRlY3RfZG9jdW1lbnRfdXJsIjoiaHR0cHM6Ly9zZGsudXMub25maWRvLmNvbSIsInN5bmNfdXJsIjoiaHR0cHM6Ly9zeW5jLm9uZmlkby5jb20iLCJob3N0ZWRfc2RrX3VybCI6Imh0dHBzOi8vaWQub25maWRvLmNvbSIsImF1dGhfdXJsIjoiaHR0cHM6Ly9hcGkudXMub25maWRvLmNvbSIsIm9uZmlkb19hcGlfdXJsIjoiaHR0cHM6Ly9hcGkudXMub25maWRvLmNvbSJ9fQ.MIGHAkIBKxH7w4ST4QkXSCuHf5CeX59WY--iJvLKHYf_gBNBXtr7i-jg7nyaEx-SUFLZLtA7XHPNXXGfprL5KEIJI6rv_QACQWbGoGRLKGlBajruJUJ5YcJqCx8m9X9HxUD7uW0fpGZKorG7ESocCql6rt3cq17rFPbvJ9lLF2YLU0Jxj3wwfUul';
       final result = await FlutterOnfido.start(
         config: OnfidoConfig(
           sdkToken:
@@ -36,6 +38,10 @@ class _MyAppState extends State<MyApp> {
             captureFace: OnfidoCaptureFaceStep(OnfidoCaptureType.PHOTO),
           ),
         ),
+        onEvent: (String name,
+            Map<String, dynamic> properties,){
+            log('foooo ' + name + properties.toString());
+        },
         iosAppearance: const OnfidoIOSAppearance(
           onfidoPrimaryColor: '#0043DF',
         ),
@@ -43,9 +49,10 @@ class _MyAppState extends State<MyApp> {
       log(result.toString());
       // ASK YOUR BACKEND IF USER HAS PASSED VERIFICATION
     } catch (error) {
+      log(error.toString());
       if(error is PlatformException){
         if(error.code == 'cancel'){
-      
+
         }
       }
     }
